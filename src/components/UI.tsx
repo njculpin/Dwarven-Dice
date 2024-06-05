88e8af3503815a0bc05682c6712753c5289f6e3b
Nicholas Culpin <njculpin@gmail.com>
2024-06-05 16:49:43 -0700

    player profiles

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -2,7 +2,7 @@ import { useGame } from "../hooks/useGame";
 import { GameContextType } from "../hooks/useGameProvider";
 
 export function UI() {
-  const { rolls, fieldGems, myGems } = useGame() as GameContextType;
+  const { rolls, fieldGems, myGems, players } = useGame() as GameContextType;
 
   const fieldBlack = fieldGems.filter((x) => x === "black").length || 0;
   const fieldBlue = fieldGems.filter((x) => x === "blue").length || 0;
@@ -19,14 +19,27 @@ export function UI() {
   return (
     <div className="fixed pointer-events-none top-0 right-0 left-0 bottom-0">
       <div className="w-full h-full flex flex-col justify-between">
-        <div className="p-8">
-          <p className="w-full text-left text-xs italic">Gems On Field</p>
-          <div className="w-full flex space-x-2">
-            <p className="font-bold">{fieldBlack}</p>
-            <p className="font-bold text-blue-500">{fieldBlue}</p>
-            <p className="font-bold text-red-500">{fieldRed}</p>
-            <p className="font-bold text-purple-500">{fieldPurple}</p>
-            <p className="font-bold text-green-500">{fieldGreen}</p>
+        <div className="p-8 w-full flex justify-between">
+          <div>
+            <p className="w-full text-left text-xs italic">Gems On Field</p>
+            <div className="w-full flex space-x-2">
+              <p className="font-bold">{fieldBlack}</p>
+              <p className="font-bold text-blue-500">{fieldBlue}</p>
+              <p className="font-bold text-red-500">{fieldRed}</p>
+              <p className="font-bold text-purple-500">{fieldPurple}</p>
+              <p className="font-bold text-green-500">{fieldGreen}</p>
+            </div>
+          </div>
+          <div className="flex flex-row space-x-2">
+            {players.map(function (player) {
+              return (
+                <div key={player.id}>
+                  <div className="w-16 h-16">
+                    <img src={player.getProfile().photo} />
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="p-8">
